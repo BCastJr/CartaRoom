56b29b319bc9629d0c673e12468dfbfc2c74bd4d
BCastJr <benedictcastresjr@gmail.com>
2019-05-09 14:10:03 -0700

    created map, current bookings, and profile dropdown components

---
+++ src/components/CurrentBooking.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "CurrentBooking"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DropProfile.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "DropProfile"
+    }
+</script>
+
+<style scoped>
+
+</style>
